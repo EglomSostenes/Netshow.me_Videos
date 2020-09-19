e2c29486b5e1bf0d64e6e5779d558bdce1b42158
eglom.sostenes <eglom.sostenes@cepedi.org.br>
2020-09-19 15:37:14 -0300

    Melhorias na navbar e verificação se a url é uma url válida

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,13 +2,24 @@ class Video < ApplicationRecord
     validates :name, presence: true
     validates :url, presence: true
     validates :user, presence: true
-    validate :verify_url
+    validate :verify_extension
+    validate :url_valid?
 
     belongs_to :user
     
-    def verify_url
-        unless self.url.end_with? ".m3u8"
-            errors.add(:url, { url: "Deve-se ter a extensão .m3u8" })
+    def verify_extension
+        if self.url
+            unless self.url.end_with? ".m3u8"
+                errors.add(:url, { url: "Deve-se ter a extensão .m3u8" })
+            end
+        end
+    end
+
+    def url_valid?
+        if self.url
+            unless self.url.match(/^(ftp|http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix)
+                errors.add(:url, {url: "URL inválida"})
+            end
         end
     end
 end
