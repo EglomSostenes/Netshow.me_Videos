828725af8a89cc846f909291507721fa0f173f96
eglom.sostenes <eglom.sostenes@cepedi.org.br>
2020-09-19 20:55:25 -0300

    Retirando criação de user para associar com video

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe Video, type: :model do
 
   it "is valid factory_bot build" do
-    video = build(:video, user: create(:user))
+    video = build(:video)
     expect(video).to be_valid
   end
 
   it "is invalid if name blank" do
-    video = build(:video, name: "", user: create(:user))
+    video = build(:video, name: "")
     expect(video).to_not be_valid
   end
 
   it "is invalid if url blank" do
-    video = build(:video, url: "", user: create(:user))
+    video = build(:video, url: "")
     expect(video).to_not be_valid
   end
 
@@ -23,12 +23,12 @@ RSpec.describe Video, type: :model do
   end
 
   it "is invalid if the url does not end with the extension .m3u8" do
-    video = build(:video, url: FFaker::Internet.http_url.concat(".mp4"), user: create(:user))
+    video = build(:video, url: FFaker::Internet.http_url.concat(".mp4"))
     expect(video).to_not be_valid
   end
 
   it "It is invalid if the url does not match the url regex" do
-    video = build(:video, url: "ususus/aa.m3u8", user: create(:user))
+    video = build(:video, url: "ususus/aa.m3u8")
     expect(video).to_not be_valid
   end
 end
