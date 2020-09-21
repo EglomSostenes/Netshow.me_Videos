0d37dbd056581d9c36938344c0fa69615deea474
eglom.sostenes <eglom.sostenes@cepedi.org.br>
2020-09-21 13:32:46 -0300

    Colando os controllers no padrão

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :set_locale
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+    def render_404
+        redirect_to videos_path, notice: "Video não encontrado"
+    end
  
     def set_locale
         session[:locale] = params[:locale] if params[:locale].present?
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,68 +3,50 @@ class VideosController < ApplicationController
   before_action :set_video, only: [:show, :edit, :update, :destroy, :play]
   before_action :authorized?, only: [:edit, :destroy, :show]
 
-  # GET /videos
-  # GET /videos.json
   def index
     @q = Video.ransack(params[:q])
     @videos = @q.result(distinct: true).order("score DESC").paginate(page: params[:page], per_page: 7)
   end
 
   def my_videos
-    @videos = Video.where(user: current_user).order("score DESC").paginate(page: params[:page], per_page: 7)
+    @videos = current_user.videos.order("score DESC").paginate(page: params[:page], per_page: 7)
   end
 
-  # GET /videos/1
-  # GET /videos/1.json
   def show
   end
 
   def play
-    @video.score = @video.score + 1
-    @video.save
+    @video.update_attribute(:score, @video.score + 1)
   end
 
-  # GET /videos/new
   def new
     @video = Video.new
   end
 
-  # GET /videos/1/edit
   def edit
   end
 
-  # POST /videos
-  # POST /videos.json
   def create
-    @video = Video.new(video_params)
-    @video.user = current_user
+    @video = current_user.videos.new(video_params)
     respond_to do |format|
       if @video.save
         format.html { redirect_to @video, notice: 'Video was successfully created.' }
-        format.json { render :show, status: :created, location: @video }
       else
         format.html { render :new }
-        format.json { render json: @video.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /videos/1
-  # PATCH/PUT /videos/1.json
   def update
     respond_to do |format|
       if @video.update(video_params)
         format.html { redirect_to @video, notice: 'Video was successfully updated.' }
-        format.json { render :show, status: :ok, location: @video }
       else
         format.html { render :edit }
-        format.json { render json: @video.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /videos/1
-  # DELETE /videos/1.json
   def destroy
     @video.destroy
     respond_to do |format|
@@ -74,19 +56,18 @@ class VideosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_video
-      @video = Video.find(params[:id])
-    end
 
-    def authorized?
-      unless @video.user == current_user
-        redirect_to videos_url, notice: "Você não tem permissão para alterar ou destruir este vídeo!"
-      end
-    end
+  def set_video
+    @video = Video.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def video_params
-      params.require(:video).permit(:name, :url, :user_id)
+  def authorized?
+    unless @video.user == current_user
+      redirect_to videos_url, notice: "Você não tem permissão para alterar ou destruir este vídeo!"
     end
+  end
+
+  def video_params
+    params.require(:video).permit(:name, :url)
+  end
 end
